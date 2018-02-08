553d94e6deeb52c0246788678728b584a1acdf79
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-02-08 21:25:22 +0100

    add tests and coverage report

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # http-random-access
 
+[![Build Status](https://travis-ci.org/e-e-e/http-random-access.svg?branch=master)](https://travis-ci.org/e-e-e/http-random-access) [![Coverage Status](https://coveralls.io/repos/github/e-e-e/http-random-access/badge.svg?branch=master)](https://coveralls.io/github/e-e-e/http-random-access?branch=master)
+
 An implementation of [abstract-random-access](https://www.npmjs.com/package/abstract-random-access) to access content via http/s.
 Providing the same interface as [random-access-file](https://www.npmjs.com/package/random-access-file) and [random-access-memory](https://www.npmjs.com/package/random-access-memory).
 
--- tests/random-access-http.test.js
+++ tests/random-access-http.test.js
@@ -1,27 +1,48 @@
 var test = require('tape')
-var proxyquire = require('proxyquire')
+var proxyquire = require('proxyquire').noPreserveCache()
 var sinon = require('sinon')
+var stoppable = require('stoppable')
 var http = require('http')
 var port = 3000
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'HEAD') {
-    res.setHeader('Content-Type', 'plain/text')
-    res.setHeader('Accept-Ranges', 'Bytes')
-    res.end()
-    return
-  }
+var server
+
+var standardHandler = (req, res) => {
   res.setHeader('Content-Type', 'plain/text')
   res.setHeader('Accept-Ranges', 'Bytes')
-  res.end('Hello Node.js Server!')
-})
+  if (req.method === 'HEAD') {
+    return res.end()
+  }
+  var range = req.headers.range
+  if (range) {
+    var match = range.match(/bytes=(\d+)-(\d+)/)
+    if (match) {
+      var a = parseInt(match[1], 10)
+      var b = parseInt(match[2], 10)
+      var str = ''
+      for (var i = a; i <= b; i++) {
+        str += (a % 10)
+      }
+      res.write(str)
+    }
+  }
+  res.end()
+}
 
-test('setup server before tests', (t) => {
-  server.listen(port, (err) => {
-    t.error(err)
+function startServer (fn, cb) {
+  if (server && server.listening) {
+    return server.stop(() => startServer(fn, cb))
+  }
+  server = stoppable(http.createServer(fn))
+  server.listen(port, cb)
+}
+
+function stopServer (t) {
+  if (!server.listening) return t.end()
+  server.stop(() => {
     t.end()
   })
-})
+}
 
 test('it uses node http/s agent setting with keepAlive when run in node', (t) => {
   var httpStub = sinon.stub()
@@ -58,25 +79,59 @@ test('it does not use node http/s when in browser', (t) => {
   t.end()
 })
 
-test('raHttp ')
-
 test('raHttp.open() callback returns error if server does not support range requests', (t) => {
-  t.end()
+  var raHttp = require('../index.js')
+  var withoutRangeSupportHandler = (req, res) => {
+    res.end()
+  }
+  startServer(withoutRangeSupportHandler, (err) => {
+    t.error(err)
+    var ra = raHttp('without-range-support', { url: 'http://localhost:3000' })
+    ra.read(0, 10, (err, res) => {
+      t.ok(err.message.search(/Not opened/) !== -1)
+      stopServer(t)
+    })
+  })
 })
 
 test('raHttp.open() callback returns error if call to axios.head() fails', (t) => {
-  t.end()
+  var raHttp = require('../index.js')
+  var notFoundHandler = (req, res) => {
+    res.statusCode = 404
+    res.end()
+  }
+  startServer(notFoundHandler, (err) => {
+    t.error(err)
+    var ra = raHttp('not-found', { url: 'http://localhost:3000' })
+    ra.read(0, 10, (err, res) => {
+      t.ok(err.message.search(/Not opened/) !== -1)
+      stopServer(t)
+    })
+  })
 })
 
-test('raHttp.read() returns a buffer', (t) => {
-  t.end()
+test('raHttp.read() returns a buffer of length requested', (t) => {
+  var raHttp = require('../index.js')
+  startServer(standardHandler, (err) => {
+    t.error(err)
+    var ra = raHttp('test', { url: 'http://localhost:3000' })
+    ra.read(10, 20, (err, data) => {
+      t.error(err)
+      t.ok(data instanceof Buffer)
+      t.same(data.length, 20)
+      stopServer(t)
+    })
+  })
 })
 
 test('raHttp.write does not throw error', (t) => {
   var raHttp = require('../index.js')
-  var ra = raHttp('test-write', { url: 'http://localhost:3000' })
-  t.doesNotThrow(ra.write.bind(ra, 10, 'some-data'))
-  t.end()
+  startServer(standardHandler, (err) => {
+    t.error(err)
+    var ra = raHttp('test-write', { url: 'http://localhost:3000' })
+    t.doesNotThrow(ra.write.bind(ra, 10, 'some-data'))
+    stopServer(t)
+  })
 })
 
 test('raHttp.write logs with options.verbose === true', (t) => {
@@ -86,19 +141,25 @@ test('raHttp.write logs with options.verbose === true', (t) => {
       log: stub
     }
   })
-  var ra = proxyRaHttp('test-write', { url: 'http://localhost:3000', verbose: true })
-  ra.write(10, 'some-data', (err, res) => {
+  startServer(standardHandler, (err) => {
     t.error(err)
-    t.ok(stub.calledWith('trying to write', 'test-write', 10, 'some-data'))
-    t.end()
+    var ra = proxyRaHttp('test-write', { url: 'http://localhost:3000', verbose: true })
+    ra.write(10, 'some-data', (err, res) => {
+      t.error(err)
+      t.ok(stub.calledWith('trying to write', 'test-write', 10, 'some-data'))
+      stopServer(t)
+    })
   })
 })
 
 test('raHttp.del does not throw error', (t) => {
   var raHttp = require('../index.js')
-  var ra = raHttp('test-del', { url: 'http://localhost:3000' })
-  t.doesNotThrow(ra.del.bind(ra, 10, 100))
-  t.end()
+  startServer(standardHandler, (err) => {
+    t.error(err)
+    var ra = raHttp('test-del', { url: 'http://localhost:3000' })
+    t.doesNotThrow(ra.del.bind(ra, 10, 100))
+    stopServer(t)
+  })
 })
 
 test('raHttp.del logs with options.verbose === true', (t) => {
@@ -108,15 +169,13 @@ test('raHttp.del logs with options.verbose === true', (t) => {
       log: stub
     }
   })
-  var ra = proxyRaHttp('test-del', { url: 'http://localhost:3000', verbose: true })
-  ra.del(10, 100, (err, res) => {
+  startServer(standardHandler, (err) => {
     t.error(err)
-    t.ok(stub.calledWith('trying to del', 'test-del', 10, 100))
-    t.end()
+    var ra = proxyRaHttp('test-del', { url: 'http://localhost:3000', verbose: true })
+    ra.del(10, 100, (err, res) => {
+      t.error(err)
+      t.ok(stub.calledWith('trying to del', 'test-del', 10, 100))
+      stopServer(t)
+    })
   })
-})
-
-test('teardown', (t) => {
-  server.close()
-  t.end()
 })
