93af8bad74b6aaf50f2fb2680b25201f015068fa
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-02-08 12:36:44 +0100

    update to ras

--- index.js
+++ index.js
@@ -1,69 +1,73 @@
 var axios = require('axios')
-var Abstract = require('abstract-random-access')
-var inherits = require('inherits')
-var http = require('http')
-var https = require('https')
+var randomAccess = require('random-access-storage')
+var logger = require('./lib/logger')
 
-var Store = function (filename, options) {
-  if (!(this instanceof Store)) return new Store(filename, options)
-  Abstract.call(this)
-  this.axios = axios.create({
-    baseURL: options.url,
-    responseType: 'arraybuffer',
-    timeout: 60000,
-    // keepAlive pools and reuses TCP connections, so it's faster
-    httpAgent: new http.Agent({ keepAlive: true }),
-    httpsAgent: new https.Agent({ keepAlive: true }),
-    // follow up to 10 HTTP 3xx redirects
-    maxRedirects: 10,
-    maxContentLength: 50 * 1000 * 1000 // cap at 50MB
+var defaultOptions = {
+  responseType: 'arraybuffer',
+  timeout: 60000,
+  // follow up to 10 HTTP 3xx redirects
+  maxRedirects: 10,
+  maxContentLength: 50 * 1000 * 1000 // cap at 50MB,
+}
+
+if (process !== undefined && process.release.name === 'node') {
+  var http = require('http')
+  var https = require('https')
+  // keepAlive pools and reuses TCP connections, so it's faster
+  defaultOptions.httpAgent = new http.Agent({ keepAlive: true })
+  defaultOptions.httpsAgent = new https.Agent({ keepAlive: true })
+}
+
+var randomAccessHttp = function (filename, options) {
+  var _axios = axios.create(Object.assign({
+    baseURL: options.url
+  }, defaultOptions))
+  var url = options.url
+  var file = filename
+  var verbose = !!options.verbose
+
+  return randomAccess({
+    open: (req) => {
+      if (verbose) logger.log('Testing to see if server accepts range requests', url, file)
+      // should cache this
+      _axios.head(file)
+        .then((response) => {
+          if (verbose) logger.log('Received headers from server')
+          const accepts = response.headers['accept-ranges']
+          if (accepts && accepts.toLowerCase().indexOf('bytes') !== -1) {
+            return req.callback(null)
+          }
+          return req.callback(new Error('Accept-Ranges does not include "bytes"'))
+        })
+        .catch((err) => {
+          if (verbose) logger.log('Error opening', file, '-', err)
+          req.callback(err)
+        })
+    },
+    read: (req) => {
+      var headers = {
+        range: `bytes=${req.offset}-${req.offset + req.size - 1}`
+      }
+      if (verbose) logger.log('Trying to read', file, headers.Range)
+      _axios.get(file, { headers: headers })
+        .then((response) => {
+          if (verbose) logger.log('read', JSON.stringify(response.headers, null, 2))
+          req.callback(null, Buffer.from(response.data))
+        })
+        .catch((err) => {
+          if (verbose) {
+            logger.log('error', file, headers.Range)
+            logger.log(err, err.stack)
+          }
+          req.callback(err)
+        })
+    },
+    write: (req) => {
+      // This is a dummy write function - does not write, but fails silently
+      if (verbose) logger.log('trying to write', file, req.offset, req.data)
+      req.callback()
+    }
   })
-  this.url = options.url
-  this.file = filename
-  this.verbose = !!options.verbose
-  inherits(Store, Abstract)
 }
 
-Store.prototype._open = function (callback) {
-  if (this.verbose) console.log('Testing to see if server accepts range requests', this.url, this.file)
-  this.axios.head(this.file)
-    .then((response) => {
-      if (this.verbose) console.log('Received headers from server')
-      const accepts = response.headers['accept-ranges']
-      if (accepts && accepts.toLowerCase().indexOf('bytes') !== -1) {
-        return callback(null)
-      }
-      return callback(new Error('Accept-Ranges does not include "bytes"'))
-    })
-    .catch((err) => {
-      if (this.verbose) console.log('Error opening', this.file, '-', err)
-      callback(err)
-    })
-}
-
-Store.prototype._read = function (offset, length, callback) {
-  var headers = {
-    range: `bytes=${offset}-${offset + length - 1}`
-  }
-  if (this.verbose) console.log('Trying to read', this.file, headers.Range)
-  this.axios.get(this.file, { headers: headers })
-    .then((response) => {
-      if (this.verbose) console.log('read', JSON.stringify(response.headers, null, 2))
-      callback(null, Buffer.from(response.data))
-    })
-    .catch((err) => {
-      if (this.verbose) {
-        console.log('error', this.file, headers.Range)
-        console.log(err, err.stack)
-      }
-      callback(err)
-    })
-}
-
-// This is a dummy write function - does not write, but fails silently
-Store.prototype._write = function (offset, buffer, callback) {
-  if (this.verbose) console.log('trying to write', this.file, offset, buffer)
-  callback()
-}
-
-module.exports = Store
+module.exports = randomAccessHttp
